cebdee98d8f4c3fc520903b5a97d80392167039d
Howard <106501624+Howard1225621@users.noreply.github.com>
2023-01-05 17:05:06 +0800

    Update urls.py

--- Django_env/main/urls.py
+++ Django_env/main/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from . import views
-
+from django.urls import path, re_path as url
 from django.views import static 
 from django.conf import settings 
-from django.conf.urls import url 
+from . import views
 
 
 urlpatterns = [
